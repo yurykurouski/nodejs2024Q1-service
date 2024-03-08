2ab17e4893cc4145a973c76dc918fb97dca85910
Yury Kurouski <yury_kurouski@epam.com>
2024-03-08 14:14:38 +0100

    feat: update app odule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserController } from './user/user.controller';
+import { DbService } from './db/db.service';
+import { UserService } from './user/user.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, UserController],
+  providers: [AppService, DbService, UserService],
 })
 export class AppModule {}
