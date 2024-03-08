0ddfd86ed3ff4496e634947ba88836d3caaa97b8
Yury Kurouski <yury_kurouski@epam.com>
2024-03-08 21:56:28 +0100

    feat: update dtos

--- src/dto/index.ts
+++ src/dto/index.ts
@@ -1,20 +1,28 @@
 import { IsNumber, IsString } from 'class-validator';
 
-export class UserDto {
+export class BaseDTO {
+  _type: 'tdo';
+
+  constructor() {
+    this._type = 'tdo';
+  }
+}
+
+export class UserDto extends BaseDTO {
   @IsString()
   public login: string;
   @IsString()
   public password: string;
 }
 
-export class PwdDataDTO {
+export class PwdDataDTO extends BaseDTO {
   @IsString()
   public oldPassword: string;
   @IsString()
   public newPassword: string;
 }
 
-export class TrackDTO {
+export class TrackDTO extends BaseDTO {
   @IsString()
   public name: string;
   @IsNumber()
