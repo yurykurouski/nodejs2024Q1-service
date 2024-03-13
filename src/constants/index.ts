fa7d515e0d7b620e85092b4608fffe032de81704
Yury Kurouski <yury_kurouski@epam.com>
2024-03-13 23:49:48 +0100

    chore: remove unused variable

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,19 +1,6 @@
-import { AlbumEntity } from 'src/modules/album/entities/album.entity';
-import { ArtistEntity } from 'src/modules/artist/entities/artist.entity';
-import { TrackEntity } from 'src/modules/track/entities/track.entity';
-import { UserEntity } from 'src/modules/user/entities/user.entity';
-import { EDBEntryNames } from 'src/types';
-
 export const MESSAGES = {
   NOT_FOUND: 'Item not found',
   CANT_VALIDATE_DATA: 'Can`t validate data',
   WRONG_DATA: 'Provided data is wrong',
   NO_ENTRY_FOUND: 'No entry found',
-};
-
-export const MODELS = {
-  [EDBEntryNames.USERS]: UserEntity,
-  [EDBEntryNames.ARTISTS]: ArtistEntity,
-  [EDBEntryNames.ALBUMS]: AlbumEntity,
-  [EDBEntryNames.TRACKS]: TrackEntity,
 };
