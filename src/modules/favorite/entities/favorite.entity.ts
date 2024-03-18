5cef95b0940609cb85922e80864f51bdfa71423b
Yury Kurouski <yury_kurouski@epam.com>
2024-03-18 22:36:23 +0100

    feat: add favorites

--- src/modules/favorite/entities/favorite.entity.ts
+++ src/modules/favorite/entities/favorite.entity.ts
@@ -1,7 +1,5 @@
-import { HttpException, HttpStatus } from '@nestjs/common';
 import { ApiProperty } from '@nestjs/swagger';
-import { MESSAGES } from 'src/constants';
-import { EDBEntryNames } from 'src/types';
+import { Exclude } from 'class-transformer';
 
 export class FavoritesEntity {
   @ApiProperty()
@@ -10,24 +8,10 @@ export class FavoritesEntity {
   public albums: string[];
   @ApiProperty()
   public tracks: string[];
+  @Exclude()
+  favoritesId: string;
 
-  constructor() {
-    this.artists = [];
-    this.albums = [];
-    this.tracks = [];
-  }
-
-  public async addToFavs(kind: EDBEntryNames, id: string) {
-    this[kind].push(id);
-  }
-
-  public async removeFromFavs(kind: EDBEntryNames, id: string) {
-    const itemIndex = this[kind].findIndex((el: string) => el === id);
-
-    if (itemIndex < 0) {
-      throw new HttpException(MESSAGES.NOT_FOUND, HttpStatus.NOT_FOUND);
-    } else {
-      this[kind].splice(itemIndex, 1);
-    }
+  constructor(data: FavoritesEntity) {
+    Object.assign(this, data);
   }
 }
--- src/modules/favorite/favorite.controller.ts
+++ src/modules/favorite/favorite.controller.ts
@@ -3,40 +3,43 @@ import {
   Delete,
   Get,
   HttpCode,
+  HttpException,
   HttpStatus,
   Param,
   ParseEnumPipe,
   ParseUUIDPipe,
   Post,
+  UseInterceptors,
 } from '@nestjs/common';
 import { EEntityName } from 'src/types';
 import { FavoriteService } from './favorite.service';
 import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { FavoritesEntity } from 'src/modules/favorite/entities/favorite.entity';
+import { FavoritesInterceptor } from './favorite.interceptor';
 
-@ApiTags('Favorites')
 @Controller('favs')
+@UseInterceptors(FavoritesInterceptor)
+@ApiTags('Favorites')
 export class FavoriteController {
   constructor(private favoriteService: FavoriteService) {
     undefined;
   }
 
+  @Get()
   @ApiOkResponse({
     description: 'get all favorites',
     type: FavoritesEntity,
     isArray: true,
   })
-  @Get()
   public async getFavorites() {
     return await this.favoriteService.getFavorites();
   }
 
+  @Post('/:entityName/:id')
   @ApiOkResponse({
     description: 'Add entity to the favorites',
     type: null,
-    isArray: false,
   })
-  @Post('/:entityName/:id')
   public async addToFavs(
     @Param('id', ParseUUIDPipe) id: string,
     @Param(
@@ -50,14 +53,14 @@ export class FavoriteController {
     return await this.favoriteService.addToFavs(entityName, id);
   }
 
+  @Delete('/:entityName/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
   @ApiOkResponse({
     description: 'Delete entity from the favorites',
     type: null,
     isArray: false,
     status: HttpStatus.NO_CONTENT,
   })
-  @Delete('/:entityName/:id')
-  @HttpCode(HttpStatus.NO_CONTENT)
   public async removeFromFavs(
     @Param('id', ParseUUIDPipe) id: string,
     @Param(
