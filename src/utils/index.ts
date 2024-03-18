07ee6f33b84ba2ec37eb76aa9e43002a76c3f54b
Yury Kurouski <yury_kurouski@epam.com>
2024-03-18 22:37:26 +0100

    chore: remove unused excludeProperty util

--- src/utils/index.ts
+++
@@ -1,8 +0,0 @@
-export const excludeProperty = <T, Key extends keyof T>(
-  user: T,
-  keys: Key[],
-) => {
-  return Object.fromEntries(
-    Object.entries(user).filter(([key]) => !keys.includes(key as Key)),
-  );
-};
